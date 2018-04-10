72dc126e26280812251187d0754547adb6888014
leareyes22 <leandroreyes_22@hotmail.com>
2018-04-10 10:00:23 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,13 @@
+package died.guias.guia02.app;
+/**
+* @author marti
+*
+*/
+public class Hola2 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("el tiempo actual en milis es "+ System.currentTimeMillis());
+	}
+}
